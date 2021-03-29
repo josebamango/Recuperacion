f7c391d9dcdb6d57448ec276c4182618de6ddd58
josebamango <56644266+josebamango@users.noreply.github.com>
2021-03-29 20:39:31 +0200

    Ejercicio bien hecho

--- JS/ejer7/script.js
+++ JS/ejer7/script.js
@@ -1,5 +1,4 @@
-addEventListener("load", inicio, false)
-;
+addEventListener("load", inicio, false);
 
 function inicio() {
     var primera = document.getElementById('primera');
@@ -8,9 +7,9 @@ function inicio() {
     var resultado = document.getElementById('resultado');
 
     boton.addEventListener('click', function () {
-        cPrimera(primera);
-        cSegunda(segunda);
-        cIncremento(primera, segunda);
+        cPrimera(primera.value);
+        cSegunda(segunda.value);
+        cIncremento(primera.value, segunda.value);
     });
 
 
@@ -18,24 +17,24 @@ function inicio() {
 
 function cPrimera(primera) {
     if (primera < 0 || primera != 0) {
-        resultado.innerHTML = "La primera es negativa o distinta de 0";
+        resultado.innerHTML = "La primera es negativa o distinta de 0\n";
     } else {
-        resultado.innerHTML = "La primera es igual a 0";
+        resultado.innerHTML = "La primera es igual a 0\n";
     }
 }
 
 function cSegunda(segunda) {
     if (segunda >= 0) {
-        resultado.innerHTML = "La segunda es positiva";
+        resultado.innerHTML += "La segunda es positiva\n";
     } else {
-        resultado.innerHTML = "La segunda es negativa"
+        resultado.innerHTML += "La segunda es negativa\n"
     }
 }
 
 function cIncremento(primera, segunda) {
     if ((primera + 1) >= segunda) {
-        resultado.innerHTML = "Al sumar 1 a la primera iguala o supera a la segunda";
+        resultado.innerHTML += "Al sumar 1 a la primera iguala o supera a la segunda";
     } else {
-        resultado.innerHTML = "Al sumar 1 a la primera no iguala o supera a la segunda";
+        resultado.innerHTML += "Al sumar 1 a la primera no iguala o supera a la segunda";
     }
 }
