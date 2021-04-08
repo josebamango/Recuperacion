fbde67215a1f3909b082f81e6abf061605622713
josebamango <56644266+josebamango@users.noreply.github.com>
2021-04-08 21:16:04 +0200

    bonito

--- JS/Hoja2/ejer3/script.js
+++ JS/Hoja2/ejer3/script.js
@@ -3,24 +3,36 @@ addEventListener('load', inicio, false);
 function inicio() {
     var tabla = document.getElementById('tabla');
     var resultado = document.getElementById('resultado');
+    var total = document.getElementById('total')
     var arraySalas = new Array();
-    var cLibres = 0;
-    var cOcupadas = 0;
+    var tLibres = 0;
+    var tOcupados = 0;
     for (i = 0; i < 5; i++) {
         arraySalas[i] = new Array();
         tabla.innerHTML += "Sala " + (i + 1) + " => ";
+        var libres = 0;
+        var ocupados = 0;
         for (j = 0; j < 20; j++) {
             arraySalas[i][j] = Math.floor(Math.random() * 2) + 0;
-            tabla.innerHTML += +arraySalas[i][j] + " | ";
+            var imagen = document.createElement('img');
             if (arraySalas[i][j] == 0) {
-                cLibres++;
+                imagen.setAttribute('src','libre.png');
+                tabla.appendChild(imagen);
+                tLibres++;
+                libres++;
             } else {
-                cOcupadas++;
+                imagen.setAttribute('src','ocupado.png');
+                tabla.appendChild(imagen);
+                ocupados++;
+                tOcupados++;
             }
-        };
-        tabla.innerHTML += "\n";
-    };
+        }
+        ;
+        tabla.innerHTML += "<br>";
+        resultado.innerHTML += 'En la sala ' + (i + 1) + ' hay ' + libres + " ordenadores libres y  " + ocupados + " ocupados\n";
+    }
+    ;
     console.log(arraySalas);
-    console.log(cLibres);
-    resultado.innerHTML = 'Hay ' + cLibres + " salas libres y " + cOcupadas + " salas ocupadas";
+    total.innerHTML += 'Hay en total ' + tLibres + " ordenadores libres y  " + tOcupados + " ocupados";
+
 };
