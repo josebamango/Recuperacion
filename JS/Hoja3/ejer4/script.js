1c20a1a589bcd8c429fbf8ff41fca7144ca7300a
josebamango <56644266+josebamango@users.noreply.github.com>
2021-04-14 16:40:38 +0200

    mucho js

--- JS/Hoja3/ejer4/script.js
+++ JS/Hoja3/ejer4/script.js
@@ -3,28 +3,53 @@ addEventListener("load", inicio, false);
 function inicio() {
 
     var boton = document.getElementById('boton')
+    var reset = document.getElementById('reset')
+    var resultado1 = document.getElementById('resultado1');
+    var resultado2 = document.getElementById('resultado2');
+    var resultado3 = document.getElementById('resultado3');
     var array1 = new Array();
     var array2 = new Array();
+    var array3 = new Array();
 
 
     boton.addEventListener("click", function () {
-        for (i = 0; i < longitud(); i++) {
-            array1.push(valores());
-            console.log("esto es un valor " + valores());
+        var filas1 = Math.floor(Math.random() * 20);
+        var filas2 = Math.floor(Math.random() * 20);
+        array1 = [];
+        array2 = [];
+        array3 = [];
+        for (i = 0; i < filas1; i++) {
+            var numeros1 = Math.floor(Math.random() * 50);
+            array1.push(numeros1);
+            resultado1.innerHTML = array1.join('-');
         }
-        console.log("este es el array " + array1);
-        console.log("tiene estas filas " + longitud());
-    })
+        for (i = 0; i < filas2; i++) {
+            var numeros2 = Math.floor(Math.random() * 50);
+            array2.push(numeros2);
+            resultado2.innerHTML = array2.join('-');
+        }
+        if (filas1 > filas2 || filas1 == filas2) {
+            for (i = 0; i < filas1; i++) {
+                array3[i] = array1[i] + array2[i];
+                if (isNaN(array3[i])){
+                    array3[i]=array1[i];
+                }
+            }
+            resultado3.innerHTML = array3.join('-');
+        } else {
+            for (i = 0; i < filas2; i++) {
+                array3[i] = array1[i] + array2[i];
+                if (isNaN(array3[i])){
+                    array3[i]=array2[i];
+                }
+            }
+            resultado3.innerHTML = array3.join('-');
+
+        }
 
 
-    function longitud() {
-        var filas = Math.floor(Math.random() * 20);
-        return filas;
-    };
+    });
 
-    function valores() {
-        var numeros = Math.floor(Math.random() * 50);
-        return numeros;
-    }
+
 }
 
